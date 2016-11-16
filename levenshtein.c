9fd6f69681a8f36a12bdb13e0ff52ac343e2fc42
alevy <antoine.levy@tredzone.com>
2016-11-16 07:45:05 +0100

    change tab* [] to tab*

--- levenshtein.c
+++ levenshtein.c
@@ -59,51 +59,55 @@ void printTab(int* tab,int lenStr1,int lenStr2){
   int i,j;
 
 
-  for(i=1;i<lenStr1;i++){
-    for(j=1;j<lenStr2;j++){
+  for(j=0;j<lenStr2;j++){
+    for(i=0;i<lenStr1;i++){
       printf("%d", tab[i+j*lenStr1]);
     }
     printf("\n");
   }
+  printf("\n");
 }
 
-int DamerauLevenshteinDistance(char* str1, int lenStr1, char* str2, int lenStr2){
+int* DamerauLevenshteinDistance(char* str1, int lenStr1, char* str2, int lenStr2){
   // d is a table with lenStr1+1 rows and lenStr2+1 columns
-  int d[lenStr1+1][lenStr2+1];
+  int* d = malloc(sizeof(int)*(lenStr1+lenStr2));
   // i and j are used to iterate over str1 and str2
   int i, j, cost;
 
   for(i=0;i<lenStr1;i++){
-        d[i][0] = i;
+    d[i*lenStr1] = i;
   }
   for(j=0;j<lenStr2;j++){
-    d[0][j] = j;
+    d[j*lenStr2] = j;
   }
 
   // printTab(d,lenStr1,lenStr2);
   for(i=1;i<lenStr1;i++){
     for(j=1;j<lenStr2;j++){
+
+
       if(str1[i-1] == str2[j-1]){ 
         cost = 0;
       }else {
         cost = 1;
       }
-      d[i][j] = minimum(
-                          d[i-1][j] + 1,     // deletion
-                          d[i][j-1] + 1,     // insertion
-                          d[i-1][j-1] + cost   // substitution
+      d[i+j*lenStr1] = minimum(
+                          d[i-1+j*lenStr1] + 1,     // deletion
+                          d[i+j*lenStr1-1] + 1,     // insertion
+                          d[i-1+j*lenStr1-1] + cost   // substitution
                 );
       if(i > 1 && j > 1 && str1[i-1] == str2[j-2] && str1[i-2] == str2[j-1]){
-                d[i][j] = minimum2(
-                                    d[i][j],
-                                    d[i-2][j-2] + 1      // transposition
+                d[i+j*lenStr1] = minimum2(
+                                    d[i+j*lenStr1],
+                                    d[i-2+j*lenStr1-2] + 1      // transposition
                           );
       }
 
     }
   }                                
 
-  return d[lenStr1][lenStr2];
+  printTab(d,lenStr1+1,lenStr2+1);
+  return d;
 }
 
 
@@ -114,17 +118,11 @@ int main(){
   // printf("minimum>%d<\n",minimum(2,1,3) );
   // printf("minimum>%d<\n",minimum(1,1,3) );
   // printf("minimum>%d<\n",minimum(2,3,1) );
-  int* tab;
-  int i,j;
-  for(i=0;i<3;i++){
-    for(j=0;j<3;j++){
-        tab[i+j*3] = i+j*3;
-    }
-  }
-  printTab(tab,3,3);
-  // printf("DamerauLevenshteinDistance(antoine,antine)=%d (expected 1)\n",DamerauLevenshteinDistance("antoine",7,"antine",6) );
-  // printf("DamerauLevenshteinDistance(toto,titi)=%d (expected 2)\n",DamerauLevenshteinDistance("toto",4,"titi",4) );
-  // printf("DamerauLevenshteinDistance(test,tet)=%d (expected 1)\n", DamerauLevenshteinDistance("test",4,"tet",3) );
-  // printf("DamerauLevenshteinDistance(ok,ko)=%d (expected ?2)\n",DamerauLevenshteinDistance("ok",2,"ko",2) );
-	return 0;
+
+  printf("DamerauLevenshteinDistance(antoine,antine)=%d (expected 1)\n",DamerauLevenshteinDistance("antoine",7,"antine",6) );
+  printf("DamerauLevenshteinDistance(toto,titi)=%d (expected 2)\n",DamerauLevenshteinDistance("toto",4,"titi",4) );
+  printf("DamerauLevenshteinDistance(test,tet)=%d (expected 1)\n", DamerauLevenshteinDistance("test",4,"tet",3) );
+  printf("DamerauLevenshteinDistance(ok,ko)=%d (expected ?2)\n",DamerauLevenshteinDistance("ok",2,"ko",2) );
+	
+  return 0;
 }
